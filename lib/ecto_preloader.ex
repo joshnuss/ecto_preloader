53cf59d591d4545c45d3a463c04ef3566ac65c7d
Josh Nussbaum <joshnuss@gmail.com>
2019-11-08 01:20:03 -0500

    Adds Ecto.Preloader

--- lib/ecto_preloader.ex
+++ lib/ecto_preloader.ex
@@ -1,18 +1,49 @@
-defmodule EctoPreloader do
+defmodule Ecto.Preloader do
   @moduledoc """
-  Documentation for EctoPreloader.
+  Ecto.Preloader is a module for preloading using joins.
+
+  It is more efficient than calling `Ecto.Query.Repo.preload`, as that results in extra queries.
+
+  Join/Preload combination is already supported by Ecto, but it's requires a bit of boilerplate.
+
+  This module simplifies it a bit.
+
+  ## Example using Ecto join+assoc+preload
+
+  ```
+  Invoice
+  |> join(:left, [i], assoc(i, :customer), as: :customer)
+  |> join(:left, [i], assoc(i, :lines), as: :lines)
+  |> preload([lines: l, customers: c], lines: l, customer: c)
+  |> Repo.all()
+  ```
+
+  ## Example using Ecto.Preloader
+
+  ```
+  import Ecto.Preloader
+
+  Invoice
+  |> preload_join(:customer)
+  |> preload_join(:lines)
+  |> Repo.all()
+  ```
+
   """
 
-  @doc """
-  Hello world.
+  import Ecto, only: [assoc: 2]
+  alias Ecto.Query
+  alias Ecto.Query.Builder.{Join, Preload}
 
-  ## Examples
+  defmacro preload_join(query, association) do
+    expr = quote do: assoc(l, unquote(association))
+    binding = quote do: [l]
+    preload_bindings = quote do: [{unquote(association), x}]
+    preload_expr = quote do: [{unquote(association), x}]
 
-      iex> EctoPreloader.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    query
+    |> Join.build(:left, binding, expr, nil, nil, association, nil, nil, __CALLER__)
+    |> elem(0)
+    |> Preload.build(preload_bindings, preload_expr, __CALLER__)
   end
 end
