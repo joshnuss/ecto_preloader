b68de249fdae32d63fcd5c63492ec64d0c122e72
Josh Nussbaum <joshnuss@gmail.com>
2019-11-08 01:19:28 -0500

    Adds project info to mix file

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule EctoPreloader.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      name: "Ecto.Preloader",
+      source_url: "https://github.com/joshnuss/ecto_preloader",
+      homepage_url: "https://github.com/joshnuss/ecto_preloader",
+      docs: [
+        main: "Ecto.Preloader",
+        extras: ["README.md"]
+      ]
     ]
   end
 
